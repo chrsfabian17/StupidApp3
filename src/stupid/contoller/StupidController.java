c0d97cdec286076bc377394fb71994ed74a493af
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-06 09:59:42 -0600

    Made app less stupid

---
+++ src/stupid/contoller/StupidController.java
@@ -0,0 +1,11 @@
+package stupid.contoller;
+
+public class StupidController 
+{
+ public void start()
+ {
+	 System.out.println("this program is less stupid");
+	 
+ }
+}
+
---
+++ src/stupid/contoller/StupidRunner.java
@@ -0,0 +1,10 @@
+package stupid.contoller;
+
+public class StupidRunner
+{
+	  public static void main (String [] args)
+	   {
+		   StupidController firstAppController = new StupidController();
+		   firstAppController.start();
+	   }
+}
